f57c5f8a62123f119b4f67fb989580341c838c1c
Filip Koudelníček <filipkoudelnicek@seznam.cz>
2026-09-04 20:39:51 +0200

    Add local font files for Barlow Condensed and Inter with appropriate unicode ranges

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ \App\Models\Setting::get('favicon_updated_at', '1') }}" sizes="any">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500;600;700;800;900&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    {{-- Fonty používané v hlavičce a první obrazovce. --}}
+    <link rel="preload" href="{{ asset('assets/fonts/barlow-condensed-700-latin.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/barlow-condensed-800-latin.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/barlow-condensed-900-latin.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/barlow-condensed-900-latin-ext.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/inter-latin-variable.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/inter-latin-ext-variable.woff2') }}" as="font" type="font/woff2" crossorigin>
 
     @yield('seo')
     <title>@yield('title') | {{ config('app.name') }}</title>
